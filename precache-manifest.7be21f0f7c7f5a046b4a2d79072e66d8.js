418c4838a5160457c16d3e27ddfcc51fdd2a5b2c
ConanKeaveney <conankeaveney96@gmail.com>
2019-11-01 00:14:45 +0000

    Updates

---
+++ precache-manifest.7be21f0f7c7f5a046b4a2d79072e66d8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3e9d44d2f2b7e0f50b0",
+    "url": "/mongo-oAuth/static/js/main.e3e9d44d.chunk.js"
+  },
+  {
+    "revision": "1c0258792c7fa6124927",
+    "url": "/mongo-oAuth/static/js/1.1c025879.chunk.js"
+  },
+  {
+    "revision": "52bd2e84f5baff81081e",
+    "url": "/mongo-oAuth/static/js/runtime~main.52bd2e84.js"
+  },
+  {
+    "revision": "e7f7c71589a62d880cb86d269400056c",
+    "url": "/mongo-oAuth/index.html"
+  }
+];
